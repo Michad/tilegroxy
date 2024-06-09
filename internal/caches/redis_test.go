06d520cec41f4145945f652c44497a6a8014a69a
Michael Davis <sites@michael.davis.name>
2024-06-08 20:40:20 -0400

    test: couple more redis tests

--- internal/caches/redis_test.go
+++ internal/caches/redis_test.go
@@ -2,7 +2,8 @@ package caches
 
 import (
 	"context"
-	"log"
+	"math"
+	"math/rand"
 	"slices"
 	"strconv"
 	"strings"
@@ -14,8 +15,9 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestWithContainer(t *testing.T) {
-	ctx := context.Background()
+func setupContainer(ctx context.Context, t *testing.T) (testcontainers.Container, func(t *testing.T)) {
+	t.Log("setup container")
+
 	req := testcontainers.ContainerRequest{
 		Image:        "redis:latest",
 		ExposedPorts: []string{"6379/tcp"},
@@ -25,52 +27,100 @@ func TestWithContainer(t *testing.T) {
 		ContainerRequest: req,
 		Started:          true,
 	})
-	if err != nil {
-		log.Fatalf("Could not start redis: %s", err)
+	assert.Nil(t, err)
+
+	return redisC, func(t *testing.T) {
+		t.Log("teardown container")
+
+		err := redisC.Terminate(ctx)
+		assert.Nil(t, err)
 	}
-	defer func() {
-		if err := redisC.Terminate(ctx); err != nil {
-			log.Fatalf("Could not stop redis: %s", err)
-		}
-	}()
+}
 
-	endpoint, err := redisC.Endpoint(ctx, "")
-
-	if err != nil {
-		log.Fatalf("Could not start connect to redis: %s", err)
-	}
-
+func extractHostAndPort(t *testing.T, endpoint string) HostAndPort {
 	split := strings.Split(endpoint, ":")
 	port, err := strconv.Atoi(split[1])
+	assert.Nil(t, err)
 
-	if err != nil {
-		log.Fatalf("Invalid port: %s", err)
-	}
+	return HostAndPort{Host: split[0], Port: uint16(port)}
+}
+
+func validateSaveAndLookup(t *testing.T, r *Redis) {
+
+	z := rand.Float64()*10 + 5
+
+	x := int(rand.Float64() * math.Exp2(z))
+	y := int(rand.Float64() * math.Exp2(z))
+	tile := internal.TileRequest{LayerName: "test", Z: int(z), X: x, Y: y}
+	img := []byte{1, 2, 3, 4, 5}
+
+	err := r.Save(tile, &img)
+	assert.Nil(t, err)
+
+	img2, err := r.Lookup(tile)
+	assert.Nil(t, err)
+
+	assert.True(t, slices.Equal(img, *img2), "Result before and after cache don't match")
+}
+
+func TestWithContainerHostAndPort(t *testing.T) {
+	ctx := context.Background()
+	redisC, cleanupF := setupContainer(ctx, t)
+
+	defer cleanupF(t)
+
+	endpoint, err := redisC.Endpoint(ctx, "")
+	assert.Nil(t, err)
 
 	config := RedisConfig{
-		HostAndPort: HostAndPort{Host: split[0], Port: uint16(port)},
+		HostAndPort: extractHostAndPort(t, endpoint),
 	}
 
 	r, err := ConstructRedis(&config, nil)
+	assert.Nil(t, err)
 
-	if err != nil {
-		log.Fatalf("Could not create cache: %s", err)
+	validateSaveAndLookup(t, r)
+}
+
+func TestWithContainerSingleServersArr(t *testing.T) {
+	ctx := context.Background()
+	redisC, cleanupF := setupContainer(ctx, t)
+
+	defer cleanupF(t)
+
+	endpoint, err := redisC.Endpoint(ctx, "")
+	assert.Nil(t, err)
+
+	config := RedisConfig{
+		Servers: []HostAndPort{extractHostAndPort(t, endpoint)},
 	}
 
-	tile := internal.TileRequest{LayerName: "test", Z: 0, X: 0, Y: 0}
-	img := []byte{1, 2, 3, 4, 5}
+	r, err := ConstructRedis(&config, nil)
+	assert.Nil(t, err)
 
-	err = r.Save(tile, &img)
+	validateSaveAndLookup(t, r)
+}
 
-	if err != nil {
-		log.Fatalf("Could not save cache: %s", err)
+func TestWithContainerRing(t *testing.T) {
+	ctx := context.Background()
+	redisC, cleanupF := setupContainer(ctx, t)
+	redisC2, cleanupF2 := setupContainer(ctx, t)
+
+	defer cleanupF(t)
+	defer cleanupF2(t)
+
+	endpoint, err := redisC.Endpoint(ctx, "")
+	assert.Nil(t, err)
+	endpoint2, err := redisC2.Endpoint(ctx, "")
+	assert.Nil(t, err)
+
+	config := RedisConfig{
+		Mode:    ModeRing,
+		Servers: []HostAndPort{extractHostAndPort(t, endpoint), extractHostAndPort(t, endpoint2)},
 	}
 
-	img2, err := r.Lookup(tile)
+	r, err := ConstructRedis(&config, nil)
+	assert.Nil(t, err)
 
-	if err != nil {
-		log.Fatalf("Could not lookup cache: %s", err)
-	}
-
-	assert.True(t, slices.Equal(img, *img2), "Result before and after cache don't match")
+	validateSaveAndLookup(t, r)
 }
