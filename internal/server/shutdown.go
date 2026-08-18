d373578501797bdca84d76e3bacdb3ad156d8764
Michael Davis <michael@michael.davis.name>
2026-08-17 21:57:49 -0400

    docs: fix copyright date for new files (copy/paste)

--- internal/server/shutdown.go
+++ internal/server/shutdown.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Michael Davis
+// Copyright 2026 Michael Davis
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
