4e9054b49a3ce0b1bfb54d31babd1f86fa2e3341
Michael Davis <git@michaeldavis.email>
2026-09-20 23:36:20 -0400

    fix: clean up stray characters in error messages

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -322,13 +322,13 @@ func (c Config) Validate() error {
 	switch c.Error.Mode {
 	case ModeErrorPlainText, ModeErrorNoError, ModeErrorImage, ModeErrorImageHeader:
 	default:
-		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "error.mode %q", c.Error.Mode))
+		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "error.mode", c.Error.Mode))
 	}
 
 	if _, ok := CustomLogLevel[strings.ToLower(c.Logging.Main.Level)]; !ok {
 		var level slog.Level
 		if err := level.UnmarshalText([]byte(c.Logging.Main.Level)); err != nil {
-			errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.main.level %q", c.Logging.Main.Level))
+			errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.main.level", c.Logging.Main.Level))
 		}
 	}
 
@@ -341,19 +341,19 @@ func (c Config) Validate() error {
 	switch c.Logging.Main.Format {
 	case MainFormatPlain, MainFormatJSON:
 	default:
-		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.main.format %q", c.Logging.Main.Format))
+		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.main.format", c.Logging.Main.Format))
 	}
 
 	switch c.Logging.Access.Format {
 	case AccessFormatCommon, AccessFormatCombined:
 	default:
-		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.access.format %q", c.Logging.Access.Format))
+		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.access.format", c.Logging.Access.Format))
 	}
 
 	switch c.Logging.Audit.Format {
 	case AuditFormatPlain, AuditFormatJSON:
 	default:
-		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.audit.format %q", c.Logging.Audit.Format))
+		errs = append(errs, fmt.Errorf(c.Error.Messages.InvalidParam, "logging.audit.format", c.Logging.Audit.Format))
 	}
 
 	if c.Server.Timeout == 0 {
